77905c1f5a3fb5f490c6ae0ab25210d492b38095
BugHunter-X <askzenai@gmail.com>
2025-01-30 16:30:07 +0100

    Add bug.vue file

---
+++ bug.vue
@@ -0,0 +1,16 @@
+This error occurs when using Tailwind CSS with a framework like Vue.js or React.js, and you're trying to apply a Tailwind utility class to an element that hasn't been properly rendered or mounted yet.  For example, if you try to add a class in the `data` or `props` section of your Vue component before the component's template is rendered, Tailwind won't be able to apply it correctly. This results in the styles not being applied or unexpected behavior.  
+```vue
+<template>
+  <div :class="['bg-blue-500', dynamicClass]"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dynamicClass: 'text-white' // This might not work as intended
+    };
+  }
+};
+</script>
+```
